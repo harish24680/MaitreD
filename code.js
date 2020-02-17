f8d5e7b028066cde77b99e004e61eac4ad8fac7a
harish24680 <60013824+harish24680@users.noreply.github.com>
2020-02-17 13:15:25 -0600

    Commit

--- code.js
+++ code.js
@@ -1,7 +1,8 @@
+
 const total = document.querySelector('#total');
 const tip = document.querySelector('#tip');
 const rate = document.querySelector('#rate');
-const tax = document.querySelector('#tax');
+// const tax = document.querySelector('#tax');
 const grandtotal = document.querySelector('#grandtotal');
 const btn = document.querySelector('#btn');
 
@@ -9,11 +10,25 @@ const btn = document.querySelector('#btn');
 btn.addEventListener("click", calc);
 
 function calc(){
-
-    // e.preventDefault();
+    if (total.value=='' || rate.value==''||total.value<=0||rate.value<0){
+        if (total.value== ''){
+            alert('Please Enter Total Value');
+        }
+        else if(total.value<=0){
+            alert('Total value should be above zero');
+        }
+        if (rate.value== ''){
+            alert('Please Enter Rate Value');
+        }
+        else if (rate.value<0){
+            alert('Rate value shouldn\'t be negative value');
+        }
+    }
+    else{
     let calctip = rate.value*total.value/100.0;
     tip.textContent = 'Tip: $' + calctip;
-    calctotal = (rate.value*total.value/100.0)+Number(total.value)+5.50;
+    calctotal = (rate.value*total.value/100.0)+Number(total.value)+(0.05*total.value);
     grandtotal.textContent = 'Grand Total: $'+ calctotal;
+    }
 }
 
